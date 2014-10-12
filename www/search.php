bafca41e54c68f5df8226306b9c3a45606f0d8f4
deltj <deltj@outlook.com>
2014-10-12 15:48:14 +0000

    searching by name or bonus works, but searching by name and bonus does not

--- www/search.php
+++ www/search.php
@@ -7,6 +7,8 @@ parse_str($serializedData,$unserializedData);
 $name = $unserializedData["itemname"];
 //print_r($name);
 
+$bonus1id = $unserializedData["bonus1"];
+
 require('creds.php');
 
 $mysqli = new mysqli($dbhost,
@@ -20,14 +22,37 @@ if (mysqli_connect_errno()) {
 	echo "connect error!";
 }
 
-$sql = "SELECT item_id,name FROM item WHERE name LIKE '%$name%';";
+$sql = "";
+/*
+$sql = "SELECT item.*, bonus.name as bonusname, item_bonuses.amount as amount " .
+	"FROM item " .
+	"INNER JOIN item_bonuses ON item.item_id = item_bonuses.item_id " .
+	"INNER JOIN bonus ON bonus.bonus_id = item_bonuses.bonus_id " .
+	"WHERE item.item_id=$item_id;";
+*/
 
-if($result = $mysqli->query($sql)) {
-	$rows = array();
-	while($r = $result->fetch_assoc()) {
-		$rows[] = $r;
+if(strlen($name) > 0) {
+	if($bonus1id > 0) {
+		// search by name and bonus1id
+	} else {
+		// search by name only
+		$sql = "SELECT item_id,name FROM item WHERE name LIKE '%$name%';";
 	}
-	print json_encode($rows);
+} else {
+	// search bu bonus1id only
+	$sql = "SELECT item.item_id, item.name FROM item_bonuses " .
+		"INNER JOIN item ON item_bonuses.item_id = item.item_id " .
+		"WHERE item_bonuses.bonus_id = $bonus1id";
+}
+
+if(strlen($sql) > 0) {
+	if($result = $mysqli->query($sql)) {
+		$rows = array();
+		while($r = $result->fetch_assoc()) {
+			$rows[] = $r;
+		}
+		print json_encode($rows);
+	}
 }
 
 $mysqli->close();
--- www/searchform.php
+++ www/searchform.php
@@ -1,78 +1,127 @@
 <!doctype HTML>
+<?php
+require('creds.php');
+
+$mysqli = new mysqli($dbhost,
+	$dbuser, 
+	$dbpass,
+	$dbname,
+	$dbport);
+
+// check for connection error
+if (mysqli_connect_errno()) {
+	echo "connect error!";
+}
+?>
 <html>
 <head>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 <script>
-$(document).ready(function() {
-	// add a handler to the itemname textbox's keyup event.  this is a 
-	// better event to use than change, because it doesn't require 
-	// the control to lose focus.
-	$('#itemname').keyup(function() {
+// this function serializes the search form data and sends it to the back end
+// search processor (search.php)
+function doSearch() {
+	//console.log("in doSearch");
 
-		// figure out what the user type in for the name
-		var itemname = document.getElementById("itemname").value;
-		//console.log(itemname);
+	// figure out what the user typed in for the name
+	var itemname = document.getElementById("itemname").value;
+	//console.log(itemname);
+
+	// figure out what the user selected for the first bonus
+	var bonus1id = document.getElementById("bonus1").selectedIndex;
+	//console.log(bonus1id);
+
+	// if the form is empty, clear the result table
+	if(bonus1id == 0 && itemname.length == 0) {
+		console.log("clearing result table");
 
 		// get a handle to the result table
 		var table = document.getElementById("resulttable");
 
-		// an empty query will return a result set containing the entire
-		// database, which we do not want.
-		if(itemname.length == 0) {
-			// itemname textbox is empty, clear the table
-			for(var j=0; j<table.rows.length; j++) {
-				table.deleteRow(j);
-			}
-		} else {
-			// itemname textbox contains some text
-
-			// serialize the search parameters so they can be sent to the 
-			// back end search processor
-			var searchparams = $("form").serialize();
-			//console.log(searchparams);
-
-			// post the search parameters to search.php and deal with the
-			// result.  if matching items are found, the result will be
-			// an array of JSON objects.
-			$.post("search.php", {data:searchparams},
-					function(data, status) {
-				//console.log(data);
-
-				// parse the JSON result so it can be displayed
-				var result = $.parseJSON(data);
-				//console.log(obj[0].name);
-				//console.log(result.length);
-
-				// loop through the array of objects
-				for(var i=0; i<result.length; i++) {
-					//console.log(result[i].name);
-
-					// now build some HTML to display the result
-
-					// first delete all rows from the table
-					for(var j=0; j<table.rows.length; j++) {
-						table.deleteRow(j);
-					}
-
-					// append a row to the table
-					var row = table.insertRow(-1);
-
-					// add a cell to the row for this item's name
-					var cell = row.insertCell(0);
-					cell.innerHTML = "<a href=\"showitem.php?id=" +
-						result[i].item_id + "\">" + result[i].name +
-						"</a>";
-				}
-			});
+		// form is empty, clear the table
+		for(var j=0; j<table.rows.length; j++) {
+			table.deleteRow(j);
 		}
-	})
+	}
+
+	// if the form is populated, do the search!
+	else {
+		// serialize the search parameters so they can be sent to the 
+		// back end search processor
+		var searchparams = $("form").serialize();
+		//console.log(searchparams);
+
+		// post the search parameters to search.php.  if matching items
+	   	// are found, the result will be an array of JSON objects.
+		$.post("search.php", {data:searchparams}, searchCallback);
+	}
+}
+
+// this function will be called by jQuery.post if the request succeeds.  It will
+// examine the result from search.php and display the result.
+//
+// @param data			the response data from search.php
+// @param status		the status of the HTTP Post
+function searchCallback(data, status) {
+	//console.log("in searchCallback");
+	console.log(data);
+
+	// parse the JSON result so it can be displayed
+	var result = $.parseJSON(data);
+	//console.log(obj[0].name);
+	//console.log(result.length);
+
+	// get a handle to the result table
+	var table = document.getElementById("resulttable");
+
+	// loop through the array of objects
+	for(var i=0; i<result.length; i++) {
+		//console.log(result[i].name);
+
+		// first delete all rows from the table
+		for(var j=0; j<table.rows.length; j++) {
+			table.deleteRow(j);
+		}
+
+		// append a row to the table (-1 means insert the new row at the end)
+		var row = table.insertRow(-1);
+
+		// add a cell to the row for this item
+		var cell = row.insertCell(0);
+		cell.innerHTML = "<a href=\"showitem.php?id=" +
+			result[i].item_id + "\">" + result[i].name +
+			"</a>";
+	}
+}
+
+// perform some initialization stuff after the document is loaded
+$(document).ready(function() {
+	// add a handler to the itemname textbox's keyup event.  this is a 
+	// better event to use than change, because it doesn't require 
+	// the control to lose focus.
+	document.getElementById("itemname").onkeyup = function() {doSearch()};
+
+	document.getElementById("bonus1").onchange = function() {doSearch()};
 });
 </script>
 
 </head>
 <body>
 <form>
+Bonus: <select name="bonus1" id="bonus1">
+<option value="0">-</option><!-- default, don't actually search for this -->
+<?php
+$sql = "SELECT * FROM bonus;";
+
+if($result = $mysqli->query($sql)) {
+	while($r = $result->fetch_assoc()) {
+		$bonusid = $r["bonus_id"];
+		print "<option value=\"$bonusid\">" . $r["name"] . "</option>\n";
+	}
+}
+$mysqli->close();
+?>
+</select><br />
 Name: <input type="text" name="itemname" id="itemname"><br />
 </form>
 
